7d4aefd13bf5b8a0f9f1f1149bab5199dcc400e6
Steffen Ricklin <steffenricklin@gmail.com>
2020-12-01 11:58:13 +0100

    adding the data loading and preprocessing from A1

---
+++ project.r
@@ -0,0 +1,112 @@
+### Install Packages
+# install.packages('remotes', dependencies = TRUE)
+# install.packages('bnlearn', dependencies=TRUE)
+# install.packages('pROC', dependencies=TRUE)
+# install.packages('ggplot2', dependencies=TRUE)
+# install.packages('caret', dependencies=TRUE)
+# install.packages("dagitty", dependencies = TRUE)
+# remotes::install_github("jtextor/bayesianNetworks")
+
+### Libraries
+library(dagitty)
+library(bayesianNetworks)
+library(bnlearn)
+library(pROC)
+library(ggplot2)
+library(caret)
+
+
+### Data
+data <- read.csv("data/processed_cleveland.csv", header = FALSE)
+colnames(data) <- c("age", "sex", "chest_pain", "rest_blood_press", 
+                    "cholesterol", "fasting_blood_sugar", "rest_ecg", 
+                    "max_heart_rate", "exercise_induced_angina", 
+                    "ST_depression", "ST_slope", "coloured_arteries",
+                    "thalassemia", "diagnosis")
+head(data)
+
+
+
+
+### Data Inspection
+
+# Continuous Variables
+range(data$age)
+range(data$rest_blood_press)
+range(data$cholesterol)
+range(data$max_heart_rate)
+range(data$ST_depression)
+
+# Categorical Variables
+factor(data$sex)[1]
+factor(data$chest_pain)[1]
+factor(data$fasting_blood_sugar)[1]
+factor(data$rest_ecg)[1]
+factor(data$exercise_induced_angina)[1]
+factor(data$ST_slope)[1]
+factor(data$coloured_arteries)[1] 
+factor(data$thalassemia)[1] 
+factor(data$diagnosis)[1] 
+
+### Preprocessing
+
+# NANs
+nrow(data[which(data$coloured_arteries == '?'),])
+nrow(data[which(data$thalassemia == '?'),])
+
+# Set these to values that occur most in the dataset 
+counts_thal <- table(data$thalassemia)
+barplot(counts_thal) # The most occuring value is 3.0
+
+counts_col <- table(data$coloured_arteries)
+barplot(counts_col) # The most occuring value is 0.0
+
+data$coloured_arteries[which(data$coloured_arteries == '?')] <- '0.0'
+data$thalassemia[which(data$thalassemia == '?')] <- '3.0'
+
+# Convert to numeric
+data$thalassemia <- as.numeric(data$thalassemia)
+data$coloured_arteries <- as.numeric(data$coloured_arteries)
+data$diagnosis <- as.numeric(data$diagnosis)
+
+### Dealing with different types of data
+
+# Convert continuous data to categorical data
+data$age <- as.numeric(cut(data$age, 5))
+data$rest_blood_press <- as.numeric(cut(data$rest_blood_press, c(90, 120, 140, 200), labels = c(1,2,3)))
+data$cholesterol <- as.numeric(cut(data$cholesterol, c(100, 200, 300, 600), labels = c(1,2,3)))
+data$max_heart_rate <- as.numeric(cut(data$max_heart_rate, c(50, 110, 140, 175, 210), labels = c(1,2,3,4)))
+data$ST_depression <- as.numeric(cut(data$ST_depression, c(-0.1, 0.0, 2, 6.5), labels = c(0,1,2)))
+
+# Bin diagnosis
+data$diagnosis[which(data$diagnosis > 0)] <- 1 
+
+
+head(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
