9fc25441c071f15b8fe683528f0fd438ebc82a49
Steffen Ricklin <steffenricklin@gmail.com>
2020-12-08 10:08:57 +0100

    copied analysis from previous project into this branch. does not work yet

--- project.r
+++ project.r
@@ -107,6 +107,90 @@ plot(tabu_3)
 
 
 
+### convert tabu_3 to dagitty structure ...
+
+
+
+
+
+
+
+
+
+### Test Network Structure 
+impliedConditionalIndependencies(tabu_3)
+
+# Chi-squared Test (only for categorical variables)
+localTests(pruned_net, data, type="cis.chisq", max.conditioning.variables = 4)
+
+### Edge Coefficients
+edges = ""
+for( x in names(net) ){
+  px <- dagitty::parents(net, x)
+  for( y in px ){
+    tst <- ci.test( x, y,setdiff(px,y), data=data )
+    
+    # Print edges
+    print(paste(y,'->',x, tst$statistic, tst$p.value ) )
+    
+    # Determine edges to make the pruned net
+    # if(tst$p.value < 0.05){
+    # edges <- paste(edges,y,'->',x, '[beta = "',round(tst$statistic, digits = 2),'"]\n')
+    # }
+  }
+}
+cat(edges)
+
+### Cross Validation
+k = 10
+folds = createFolds(data$sex, k = k)
+all_preds <- NULL
+all_labels <- NULL
+for (test_index in folds) {
+  # Split data into test and train
+  train_index <- setdiff(1:nrow(data), test_index)
+  test_data = data[test_index,]
+  train_data = data[train_index,]
+  
+  # Convert model to bnlearn
+  net_bn <- model2network(toString(pruned_net,"bnlearn")) 
+  
+  # Fit on data
+  fit <- bn.fit(net_bn, train_data); fit
+  
+  # Predict 
+  preds <- predict(fit, node= 'diagnosis', data = test_data, method = "bayes-lw", n = 10000) 
+  
+  # Save all data
+  all_preds <- c(all_preds, preds)
+  all_labels <- c(all_labels, test_data$diagnosis)
+}
+
+### Analysis
+
+# Check range
+range(all_preds)
+
+# Round values
+all_preds = round(all_preds)
+
+# ROC & AUC
+png("plots/prunednet_roc.png")
+plot(roc(all_preds, all_labels))
+dev.off()
+auc(all_preds, all_labels)
+
+# Confusion Matrix
+cm <- confusionMatrix(data = factor(all_preds), reference = factor(all_labels)); cm
+png("plots/prunednet_confusion_matrix.png", width = 650)
+ggplot(data = as.data.frame(cm$table), aes(sort(Reference,decreasing = T), Prediction, fill= Freq)) +
+  geom_tile() + geom_text(aes(label=Freq), size = 7) +
+  scale_fill_gradient(low="white", high="#B4261A") +
+  labs(x = "Ground Truth",y = "Prediction", fill="Frequency", title = "Pruned Bayesian Network Predictions", size=8) +
+  scale_x_discrete(labels=c("Heart Disease", "No Heart Disease")) +
+  scale_y_discrete(labels=c("No Heart Disease", "Heart Disease")) +
+  theme_bw(base_size = 15)
+dev.off()
 
 
 
